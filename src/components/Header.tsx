38651f370f324895f0d33761f4c8437481b3ae1e
victoroliveira13 <victor12321@hotmail.com.br>
2023-05-28 23:18:23 -0300

    style():;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,13 @@ import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 import { AuthContext } from '../contexts/AuthContext';
 
-const navigation = ['Dashboard', 'Team', 'Projects', 'Calendar', 'Reports'];
+const navigation = [
+  { label: 'Dashboard', route: '/' },
+  { label: 'Team', route: '#' },
+  { label: 'Projects', route: '#' },
+  { label: 'Calendar', route: '#' },
+  { label: 'Reports', route: '#' },
+];
 const profile = ['Your Profile', 'Settings'];
 
 function classNames(...classes) {
@@ -39,24 +45,24 @@ export default function Header () {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-10 flex items-baseline space-x-4">
-                      {navigation.map((item, itemIdx) =>
-                        itemIdx === 0 ? (
-                          <Fragment key={item}>
-                            {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                            <a href="#" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
-                              {item}
-                            </a>
-                          </Fragment>
-                        ) : (
-                          <a
-                            key={item}
-                            href="#"
-                            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                          >
-                            {item}
+                    {navigation.map((item, itemIdx) =>
+                      itemIdx === 0 ? (
+                        <Fragment key={item.label}>
+                          {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                          <a href={item.route} className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                            {item.label}
                           </a>
-                        )
-                      )}
+                        </Fragment>
+                      ) : (
+                        <a
+                          key={item.label}
+                          href={item.route}
+                          className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                        >
+                          {item.label}
+                        </a>
+                      )
+                    )}
                     </div>
                   </div>
                 </div>
@@ -142,24 +148,24 @@ export default function Header () {
 
             <Disclosure.Panel className="md:hidden">
               <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                {navigation.map((item, itemIdx) =>
-                  itemIdx === 0 ? (
-                    <Fragment key={item}>
-                      {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                      <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">
-                        {item}
-                      </a>
-                    </Fragment>
-                  ) : (
-                    <a
-                      key={item}
-                      href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                    >
-                      {item}
+              {navigation.map((item, itemIdx) =>
+                itemIdx === 0 ? (
+                  <Fragment key={item.label}>
+                    {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                    <a href={item.route} className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">
+                      {item.label}
                     </a>
-                  )
-                )}
+                  </Fragment>
+                ) : (
+                  <a
+                    key={item.label}
+                    href={item.route}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    {item.label}
+                  </a>
+                )
+              )}
               </div>
               <div className="pt-4 pb-3 border-t border-gray-700">
                 <div className="flex items-center px-5">
